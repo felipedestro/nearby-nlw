9feffb80cf8fe40df12c4b3bfd5aaed3e0707ee6
Felipe Destro <felipe.s.destro@gmail.com>
2024-12-11 17:27:37 -0400

    feat: finally project

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,11 +1,24 @@
-import { View, Alert } from "react-native";
-import { api } from "@/services/api";
 import { useEffect, useState } from "react";
-import { Categories, categoriesProps } from "@/components/categories";
+import { View, Text, Alert } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+
+import { api } from "@/services/api";
+import { colors, fontFamily } from "@/styles/theme";
+
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
+import { Categories, categoriesProps } from "@/components/categories";
+import { router } from "expo-router";
 
-type marketsProps = PlaceProps & {};
+type marketsProps = PlaceProps & {
+  latitude: number;
+  longitude: number;
+};
+
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+};
 
 export default function Home() {
   const [categories, setCategories] = useState<categoriesProps>([]);
@@ -50,6 +63,60 @@ export default function Home() {
         onSelected={setCategory}
         selected={category}
       />
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
+
+        {markets.map((item) => (
+          <Marker
+            key={item.id}
+            identifier={item.id}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
+            image={require("@/assets/pin.png")}
+          >
+            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                  {item.name}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >
+                  {item.address}
+                </Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
 
       <Places data={markets} />
     </View>
