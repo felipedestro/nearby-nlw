38cb5288d48e03d4a0344d5ac3dcc563ce1c1607
Felipe Destro <felipe.s.destro@gmail.com>
2024-12-11 11:09:23 -0400

    feat: add components for home page

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,9 +1,57 @@
-import { Text, View } from "react-native";
+import { View, Alert } from "react-native";
+import { api } from "@/services/api";
+import { useEffect, useState } from "react";
+import { Categories, categoriesProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+
+type marketsProps = PlaceProps & {};
 
 export default function Home() {
+  const [categories, setCategories] = useState<categoriesProps>([]);
+  const [category, setCategory] = useState("");
+  const [markets, setMarkets] = useState<marketsProps[]>([]);
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories");
+      setCategories(data);
+      setCategory(data[0].id);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Categorias", "Não foi possível carregar as categorias.");
+    }
+  }
+
+  async function fetchMarkets() {
+    try {
+      if (!category) return;
+
+      const { data } = await api.get("/markets/category/" + category);
+      setMarkets(data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Locais", "Não foi possível carregar os lugares.");
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  useEffect(() => {
+    fetchMarkets();
+  }, [category]);
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Home</Text>
+    <View style={{ flex: 1, backgroundColor: "#ccc" }}>
+      <Categories
+        data={categories}
+        onSelected={setCategory}
+        selected={category}
+      />
+
+      <Places data={markets} />
     </View>
   );
 }
